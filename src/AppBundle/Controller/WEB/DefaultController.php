eae71efee4b7a762b2ec3693b5380907a9217fe3
klezaic <kresimir.lezaic90@gmail.com>
2017-04-24 15:23:42 +0200

    tasks 1-3 completed

--- src/AppBundle/Controller/WEB/DefaultController.php
+++ src/AppBundle/Controller/WEB/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\URIRating;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,8 +15,26 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
+/*
+        //izrada novog entiteta
+        $URIRating = new URIRating();
+        $URIRating->setUri("www.google.com");
+        $URIRating->setVisitorId("000abc");
+        $URIRating->setRating(9);
+        //dodavanje elementa
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($URIRating);
+        $em->flush();
+*/
+        //Dohvacanje svih entiteta
+        $URIRatings = $this->getDoctrine()->getRepository('AppBundle\Entity\URIRating')->findAll();
+
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'URIRatingsCount' => strval(count($URIRatings)),
+
         ));
+
     }
+
 }
